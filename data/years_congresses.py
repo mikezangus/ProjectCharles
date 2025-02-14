31eccea484a96730ad17e28af5d711246df2d64a
mikezangus <zangusm@gmail.com>
2025-02-14 15:58:36 -0500

    compatible with american millennium

--- data/years_congresses.py
+++ data/years_congresses.py
@@ -3,20 +3,20 @@ import os
 from typing import Dict, List
 
 
-START_CONGRESS = 1
-END_CONGRESS = 501
 START_YEAR = 1789
+END_YEAR = 2789
+START_CONGRESS = 1
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-FILE_PATH = os.path.join(CURRENT_DIR,  "congresses-years.json")
+FILE_PATH = os.path.join(CURRENT_DIR,  "years-congresses.json")
 
 
 def generate() -> Dict[int, List[int]]:
     dict = {}
-    congress = START_CONGRESS
     year = START_YEAR
-    for congress in range(START_CONGRESS, END_CONGRESS + 1):
-        dict[congress] = [year, year + 1]
-        year += 2
+    congress = START_CONGRESS
+    for year in range(START_YEAR, END_YEAR + 1):
+        if year % 2 == 1 and year > START_YEAR: congress += 1
+        dict[year] = congress
     return dict
 
 
