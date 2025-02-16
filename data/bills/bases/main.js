7000dc805b231dec43bb2cfbebffedd406181617
mikezangus <zangusm@gmail.com>
2025-02-15 20:09:47 -0500

    accessing bills by congress instead of all bills at once

--- data/bills/bases/main.js
+++ data/bills/bases/main.js
@@ -1,14 +1,22 @@
-const insert = require("./insert");
-const scrape = require("./scrape");
+const { spawn } = require("child_process");
+const db = require("../../db");
+const insert = require("./insertIntoDB");
+const fetch = require("./fetchBillsFromWeb");
 
 
-const CONGRESS = 119;
+const START_CONGRESS = 95;
+const END_CONGRESS = 119;
 
 
 async function main()
 {
-    const rows = await scrape(CONGRESS);
-    await insert(rows);
+    spawn("caffeinate", ["-i"]);
+    const connection = await db;
+    for (let congress = START_CONGRESS; congress <= END_CONGRESS; congress++) {
+        const rows = await fetch(congress);
+        await insert(rows, connection);
+    }
+    await connection.end();
     process.exit(0);
 }
 
