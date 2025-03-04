e3d0ccfb3b276658f36180bdc6d2fd1f8f82821d
mikezangus <zangusm@gmail.com>
2025-03-04 01:09:54 -0500

    clean last name, other error improvements

--- data/votes/fetchBioIdFromDB.js
+++ data/votes/fetchBioIdFromDB.js
@@ -1,4 +1,6 @@
 const xpath = require("xpath");
+const cleanLastName = require("../cleanLastName");
+const writeLog = require("../writeLog");
 
 
 function extractBioInfo(voteElements, chamber)
@@ -6,27 +8,32 @@ function extractBioInfo(voteElements, chamber)
     let state = null;
     let lastName = null;
     let party = null;
-    if (chamber === 'H') {
-        state = voteElements.getAttribute("state") || null;
-        lastName = voteElements.firstChild
-            ? voteElements.firstChild.nodeValue.trim()
-            : null;
-        party = voteElements.getAttribute("party") || null;
-    } else if (chamber === 'S') {
-        state = xpath.select1("string(state)", voteElements) || null;
-        lastName = xpath.select1("string(last_name)", voteElements) || null;
-        party = xpath.select1("string(party)", voteElements) || null;
-    } else {
-        throw new Error(`Invalid chamber=${chamber}`);
+    try {
+        if (chamber === 'H') {
+            state = voteElements.getAttribute("state") || null;
+            lastName = voteElements.firstChild
+                ? voteElements.firstChild.nodeValue.trim()
+                : null;
+            party = voteElements.getAttribute("party") || null;
+        } else if (chamber === 'S') {
+            state = xpath.select1("string(state)", voteElements) || null;
+            lastName = xpath.select1("string(last_name)", voteElements) || null;
+            party = xpath.select1("string(party)", voteElements) || null;
+        } else {
+            throw new Error(`Invalid value:\nchamber=${chamber}`);
+        }
+        if (!state || !lastName || !party) {
+            throw new Error(`Invalid value(s):\nstate=${state} | lastName=${lastName} | party=${party}`);
+        }
+        return { state, lastName, party };
+    } catch (error) {
+        console.error(`state=${state} lastName=${lastName} party=${party}`);
+        throw error;
     }
-    if (!state || !lastName || !party) {
-        throw new Error(`state=${state} | lastName=${lastName} | party=${party}`);
-    }
-    return { state, lastName, party };
 }
 
 
-async function fetchBioIdFromDB(connection, chamber, congress, voteElements)
+async function fetchResponse(connection, lastName, state, congress, party, chamber)
 {
     const query = `
         SELECT bio_id FROM Members
@@ -36,13 +43,35 @@ async function fetchBioIdFromDB(connection, chamber, congress, voteElements)
             AND party = ?
             AND chamber = ?
     `;
+    const [response] = await connection.execute(
+        query,
+        [lastName, state, congress, party, chamber]
+    );
+    return response.length > 0
+        ? response[0].bio_id
+        : null;
+}
+
+
+async function fetchBioIdFromDB(connection, chamber, congress, voteElements)
+{
     try {
         const { state, lastName, party } = extractBioInfo(voteElements, chamber);
-        const [response] = await connection.execute(
-            query,
-            [lastName, state, congress, party, chamber]
+        let bioID = null;
+        bioID = fetchResponse(
+            connection, lastName, state, congress, party, chamber
         );
-        return response;
+        if (bioID) {
+            return bioID;
+        }
+        bioID = fetchResponse(
+            connection, cleanLastName(lastName), state, congress, party, chamber
+        );
+        if (bioID) {
+            return bioID;
+        }
+        writeLog(`Failed to get bio_id from DB:\nlastName=${lastName} | state=${state} | congress=${congress} | party=${party} | chamber=${chamber}\nResponse:\n${response}`);
+        return null;
     } catch (error) {
         console.error(error);
         throw error;
