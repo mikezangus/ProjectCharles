66f969a216e618297f510271be57489285996c47
mikezangus <zangusm@gmail.com>
2025-03-09 22:29:44 -0400

    sped up with parallel element finds

--- data/src/bill-text/scrapeBillTextFromWeb.js
+++ data/src/bill-text/scrapeBillTextFromWeb.js
@@ -4,11 +4,17 @@ const sludge = require("../utils/sludge");
 const writeLog = require("../utils/writeLog");
 
 
-async function findTextElement(driver)
+const PDF_ONLY = 1;
+const PAGE_DOESNT_EXIST = 2;
+
+
+async function findTextElement(isOriginalURL, webDriver)
 {
-    const xpath = "/html/body/div[2]/div/main/div[2]/div[2]/div[2]/pre";
+    const originalXpath = "/html/body/div[2]/div/main/div[2]/div[2]/div[2]/pre";
+    const versionXpath = '//*[@id="billTextContainer"]'
+    const xpath = isOriginalURL ? originalXpath : versionXpath;
     try {
-        return await driver.wait(
+        return await webDriver.wait(
             until.elementLocated(By.xpath(xpath)),
             5000
         );
@@ -18,11 +24,11 @@ async function findTextElement(driver)
 }
 
 
-async function findPdfElement(driver)
+async function findPdfElement(webDriver)
 {
     const xpath = "/html/body/div[2]/div/main/div[2]/div/embed";
     try {
-        return await driver.wait(
+        return await webDriver.wait(
             until.elementLocated(By.xpath(xpath)),
             5000
         );
@@ -32,17 +38,48 @@ async function findPdfElement(driver)
 }
 
 
-async function getTextElement(webDriver, url)
+async function findVersionElement(webDriver)
+{
+    const xpath = "/html/body/div[2]/div/main/div[2]/div[2]/div[1]/ul/li[2]/a";
+    try {
+        return await webDriver.wait(
+            until.elementLocated(By.xpath(xpath)),
+            5000
+        );
+    } catch (error) {
+        return null;
+    }
+}
+
+
+async function findErrorElement(webDriver)
+{
+    const xpath = "/html/body/div[2]/div/main/main/article/h1";
+    try {
+        const element = await webDriver.wait(
+            until.elementLocated(By.xpath(xpath)),
+            5000
+        );
+        const text = await element.getText();
+        if (text.includes("couldn't find")) {
+            return PAGE_DOESNT_EXIST;
+        }
+        return null;
+    } catch (error) {
+        return null;
+    }
+}
+
+
+async function getTextElement(isOriginalURL, webDriver)
 {
     try {
-        const textElement = await findTextElement(webDriver);
-        if (textElement) {
-            return textElement;
-        }
-        const pdfElement = await findPdfElement(webDriver);
-        if (pdfElement) {
-            return 1;
-        }
+        const result = await Promise.race([
+            findTextElement(isOriginalURL, webDriver),
+            findPdfElement(webDriver).then(result => result ? PDF_ONLY : null),
+            findErrorElement(webDriver).then(result => result ? PAGE_DOESNT_EXIST : null),
+        ]);
+        return result ?? await findVersionElement(webDriver);
     } catch (error) {
         return null;
     }
@@ -52,6 +89,7 @@ async function getTextElement(webDriver, url)
 async function scrapeBillTextFromWeb(webDriverWrapper, url)
 {
     const maxAttempts = 5;
+    let privateURL = url;
     for (let attempt = 0; attempt < maxAttempts; attempt++) {
         if (attempt > 0) {
             console.log(`Attempt ${attempt + 1}/${maxAttempts}`);
@@ -61,22 +99,29 @@ async function scrapeBillTextFromWeb(webDriverWrapper, url)
             await webDriver
                 .manage()
                 .setTimeouts({ pageLoad: 10000 });
-            await webDriver.get(url);
+            await webDriver.get(privateURL);
         } catch (error) {
             continue;
         }
-        const result = await sludge(
-            () => getTextElement(webDriver, url),
-            10000
-        );
-        if (typeof result === "number" && result === 1) {
+        const result = await getTextElement(privateURL === url, webDriver);
+        if (typeof result === "number" && result === PDF_ONLY) {
             console.log("PDF only");
             writeLog(`PDF only | ${url}`);
+            return null;
+        }
+        if (typeof result === "number" && result === PAGE_DOESNT_EXIST) {
+            console.log("Page doesn't exist");
+            writeLog(`Page doesn't exist | ${url}`);
             return null;
         }
         if (!result) {
             await webDriver.quit();
             webDriverWrapper.instance = await buildWebDriver();
+            continue;
+        }
+        const href = await result.getAttribute("href") || null;
+        if (href && href.includes("bill/")) {
+            privateURL = href;
             continue;
         }
         return await result.getText();
