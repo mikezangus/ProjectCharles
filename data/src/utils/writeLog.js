d8dcc6ece5920f3dd78202cca840642d08fa3a71
mikezangus <zangusm@gmail.com>
2025-03-11 23:12:09 -0400

    formatting

--- data/src/utils/writeLog.js
+++ data/src/utils/writeLog.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
-const file = "log.log";
 
 
 function writeLog(message) {
     const timestamp = new Date().toLocaleString();
     const log = `[${timestamp}] ${message}\n`;
-    fs.appendFileSync(file, log, "utf8");
+    fs.appendFileSync("log.log", log, "utf8");
 }
 
 
