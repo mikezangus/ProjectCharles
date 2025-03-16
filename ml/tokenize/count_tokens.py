4f13c7a56e15f820eee82f61a1ca3dba3269aebe
mikezangus <zangusm@gmail.com>
2025-03-15 23:33:27 -0400

    unique token counts per bill by bill type

---
+++ ml/tokenize/count_tokens.py
@@ -0,0 +1,45 @@
+import os
+from file_manager import FileManager
+
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+BILL_TEXTS_DIR = os.path.join(PROJECT_DIR, "data", "bill-texts")
+TOKENS_RAW_DIR = os.path.join(BILL_TEXTS_DIR, "tokens_raw")
+TOKEN_COUNTS_DIR = os.path.join(BILL_TEXTS_DIR, "token_counts")
+
+
+def get_bill_types() -> list:
+    bill_types = []
+    file_manager = FileManager()
+    file_names = file_manager.get_file_names(TOKENS_RAW_DIR, extension=".txt")
+    for file_name in file_names:
+        bill_type = file_name.split('_')[1]
+        if bill_type not in bill_types:
+            bill_types.append(bill_type)
+    return bill_types
+
+
+def count_tokens():
+    file_manager = FileManager()
+    bill_types = get_bill_types()
+    for bill_type in bill_types:
+        token_counts = {}
+        file_names = file_manager.get_file_names(TOKENS_RAW_DIR, bill_type, ".txt")
+        for file_name in file_names:
+            file_path = os.path.join(TOKENS_RAW_DIR, file_name)
+            all_tokens = file_manager.load_tokens_from_txt_file(file_path)
+            unique_tokens = list(set(all_tokens))
+            for token in unique_tokens:
+                token_counts[token] = token_counts.get(token, 0) + 1
+        token_counts = dict(sorted(
+            token_counts.items(),
+            key=lambda x: x[1],
+            reverse=True
+        ))
+        save_path = os.path.join(TOKEN_COUNTS_DIR, f"{bill_type}.json")
+        file_manager.save_dict_to_file(save_path, token_counts)
+
+
+if __name__ == "__main__":
+    count_tokens()
