f2413e8b19d6952b2728995b517075228602eef2
mikezangus <zangusm@gmail.com>
2025-03-15 23:33:01 -0400

    save dicts

--- ml/tokenize/file_manager.py
+++ ml/tokenize/file_manager.py
@@ -1,13 +1,16 @@
+import json
 import os
 
 
 class FileManager:
 
     @staticmethod
-    def get_file_names(dir: str, extension: str = None) -> list[str]:
+    def get_file_names(dir: str, includes: str = None, extension: str = None) -> list[str]:
         if not os.path.exists(dir):
             raise FileNotFoundError(f"Dir not found: {dir}")
         file_names = os.listdir(dir)
+        if includes:
+            file_names = [f for f in file_names if includes.lower() in f.lower()]
         if extension:
             file_names = [f for f in file_names if f.endswith(extension)]
         return sorted(file_names)
@@ -31,7 +34,17 @@ class FileManager:
             return file.read()
     
     @staticmethod
-    def save_tokenized_file(path: str, tokens: list[str]):
+    def save_dict_to_file(path: str, content: dict):
+        if not path.endswith(".json"):
+            raise ValueError(f"File path is not to .json file: {path}")
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'w', encoding="utf=8") as file:
+            json.dump(content, file, indent=4)
+
+    @staticmethod
+    def save_tokens_to_file(path: str, content: list[str]):
+        if not path.endswith(".txt"):
+            raise ValueError(f"File path is not to .txt file: {path}")
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, "w", encoding="utf-8") as file:
-            file.write(" ".join(tokens))
+            file.write(" ".join(content))
